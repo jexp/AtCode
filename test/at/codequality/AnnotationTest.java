4e1bea4d9641f8b0dce86749b061e3bfa3bd32bc
dirk.mahler <dirk.mahler@Mythenmetz>
2010-10-23 17:41:23 +0200

    Added refactoring annotations.

--- test/at/codequality/AnnotationTest.java
+++ test/at/codequality/AnnotationTest.java
@@ -2,9 +2,16 @@ package at.codequality;
 
 public class AnnotationTest {
 
-    @Code.Smell.LongMethod(priority = Code.Priority.HIGH, rating = Code.Rating._5)
-    public void longMethod() {
-        // 1000 lines
-    }
+	@Code.Smell.LongMethod(
+			priority = Code.Priority.HIGH, 
+			rating = Code.Rating._5, 
+			comment = "This is too large...", 
+			reporter = "dirk.mahler@buschmais.com")
+	@Code.Refactoring.ExtractMethod(
+			comment="It should be split in ...", 
+			reporter="dirk.mahler@buschmais.com")
+	public void longMethod() {
+		// 1000 lines
+	}
 
 }
